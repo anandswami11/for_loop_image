d9b01795fd4b239811ee96ed4581b3b0e748287d
anand kumar <anand00kumar20@gmail.com>
2023-03-27 14:08:03 +0530

    no update loop
    no update loop

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+
+let Picture = ['<img src="images/images1.png">', '<img src="images/images2.png">', '<img src="images/images3.png">', '<img src="images/images4.png">', '<img src="images/images1.png">', '<img src="images/images2.png">', '<img src="images/images3.png">', '<img src="images/images4.png">', '<img src="images/images1.png">', '<img src="images/images2.png">']
+
+for(o = 0; o < 10; o++){
+
+document.write('<div class="hello">'+ Picture[o] +'</div>')
+}
